a819d32b66f646fb855d604300167d87ce07ab2b
Mo59471 <9674025@graniteschools.org>
2026-04-02 18:15:55 -0600

    Update Checkbox.py

--- src/Checkbox.py
+++ src/Checkbox.py
@@ -1,3 +1,13 @@
 class Checkbox:
-    def __init__(self):
+    def __init__(self, label, x, y, checkImg):
+        self.label = label
+        self.x = x
+        self.y = y
+        self.checkImg = checkImg
+        self.checked = False
+        
+    def display(self):
+        pass
+    
+    def check(self, clicked):
         pass
