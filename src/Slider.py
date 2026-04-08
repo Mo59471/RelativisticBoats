b169499b36a1e7cd9601b589308a4a862a5cf37e
Mo59471 <9674025@graniteschools.org>
2026-04-08 17:39:18 -0600

    Update Slider.py

--- src/Slider.py
+++ src/Slider.py
@@ -1,14 +1,19 @@
+import pygame
+
 class Slider:
     def __init__(self):
         self.slideX = 0
         self.v = 0
         self.hovering = False
+        self.starImg = pygame.image.load("Star.png").convert_alpha()
+
         
-    def display(self):
-        pass
-    
-    def hover(self):
-        pass
-    
-    def slide(self, clicked):
-        pass
+    def display(self, screen, clicked):
+        screen.blit(self.starImg, (25+ self.slideX, 455))
+        if clicked and self.hovering:
+            mouseX, _ = pygame.mouse.get_pos()
+            if mouseX >= 25 and mouseX + 50 <= 294:
+                self.slideX = mouseX - 25
+
+
+                
